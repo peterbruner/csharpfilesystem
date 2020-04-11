5861209080b75067cafc4c53f503c52d16774589
Peter <bruner.peter@gmail.com>
2020-04-11 11:09:58 -0400

    added logic to only create dir if it does not already exist

--- Program.cs
+++ Program.cs
@@ -8,15 +8,22 @@ namespace FileSystemTest
         static void Main(string[] args)
         {
             var app = new Program();
-            app.CreateDirectory(); // running multiple times will overwrite any preexisting instance, since it's an empty dir
+            app.CreateDirectory(); 
             Console.Read(); // forces the console to stay open
         }
 
         public void CreateDirectory()
         {
             var dirName = "TestFolder";
-
-            Directory.CreateDirectory(dirName);
+            if (Directory.Exists(dirName))
+            {
+                Console.WriteLine("Dir '" + dirName + "' exists");
+            }
+            else
+            {
+                Directory.CreateDirectory(dirName);
+                Console.WriteLine("Create Dir '" + dirName + "'");
+            }
         }
     }
 }
